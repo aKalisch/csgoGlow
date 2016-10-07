1fd143dce1274aae7b153381d1db21e0e260b2bf
Andre Kalisch <aka@rissc.com>
2016-10-07 08:03:37 +0200

    Updated Offsets 07.10.2016

--- Wall/main.cpp
+++ Wall/main.cpp
@@ -22,9 +22,9 @@
 #include "Utils.cpp"
 #include <ApplicationServices/ApplicationServices.h>
 
-uint64_t glowInfoOffset     = 0x595C570;
-uint64_t LocalPlayerBase    = 0x50B2F48;
-uint64_t playerBase         = 0x50C9818;
+uint64_t glowInfoOffset     = 0x59E9C10;
+uint64_t LocalPlayerBase    = 0x51400A8;
+uint64_t playerBase         = 0x51400A8;
 
 uint64_t m_iGlowIndex       = 0xAC10;
 bool statBool = true;
